57c539fb6e369f0fe946d9e8674bcde11aac7f4a
Aracele Torres <aracele@kde.org>
2026-06-22 20:32:17 -0300

    Load page and map styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "leaflet/dist/leaflet.css";
 import "./styles.css";
+import "./layout.css";
+import "./login.css";
+import "./home.css";
+import "./map.css";
 import App from "./App";
 
 createRoot(document.getElementById("root") as HTMLElement).render(<React.StrictMode><App /></React.StrictMode>);
